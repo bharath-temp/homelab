c5de5e2e9d06d9d3874922aedbeefee707f11c0a
Bharath Padmaraju <bharath.k.padmaraju@gmail.com>
2026-03-25 00:25:26 -0400

    fix: remove cilium stuff for now

--- bootstrap/scripts/inst-k0s-reg-controller.sh
+++ bootstrap/scripts/inst-k0s-reg-controller.sh
@@ -8,18 +8,4 @@ fi
 curl --proto '=https' --tlsv1.2 -sSf https://get.k0s.sh | sudo sh
 
 k0s config create > /etc/k0s/k0s.yaml
-
-tmpfile="$(mktemp /etc/k0s/k0s.yaml.XXXXXX)"
-
-podman run --rm \
-            --security-opt label=disable \
-            -v /etc/k0s:/workdir \
-            docker.io/mikefarah/yq:4 \
-            '
-              .spec.network.provider = "custom" |
-              .spec.network.kubeProxy.disabled = true
-            ' /workdir/k0s.yaml > "${tmpfile}"
-
-          mv "${tmpfile}" /etc/k0s/k0s.yaml
-
 k0s install controller --enable-worker --no-taints --token-file /var/lib/k0s/tokens/controller-token -c /etc/k0s/k0s.yaml --start
---
+++ docs/install-cillium.md
@@ -0,0 +1,7 @@
+# Install Cillium CNI
+
+Not in play yet, cilium doesn't work well with the current topology of control+worker nodes. 
+
+## Links
+### Official Docs
+- 
